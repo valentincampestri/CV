cd116b6a2bec8f7090a08d7d185ba4f874f3b9e5
Valentin Campestri <dev.valentincampestri@hotmail.com>
2023-12-03 16:41:35 -0300

    Se agrega la posibilidad de cambiar el color principal de los objetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,30 +23,57 @@ const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
 
-// Previously selected topic (if user selected)
+// Colores predefinidos
+const predefinedColors = ['#8a2be2', '#ff0000', '#0066ff', '#14c84a', '#ee00ff'];
+
+// Tema y icono seleccionados anteriormente (si el usuario seleccionó)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+// Obtenemos el tema actual que tiene la interfaz verificando la clase dark-theme
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
-// We validate if the user previously chose a topic
+// Si el usuario eligió un tema anteriormente, aplicamos esa elección
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
+// Activar/desactivar el tema manualmente con el botón
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+    // Guardamos el tema y el icono actual que eligió el usuario
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*=============== COLOR PICKER ===============*/
+const colorPickerIcon = document.getElementById('color-picker-icon');
+const colorMenu = document.getElementById('color-menu');
+
+// Asigna una función al evento de clic del icono de la paleta
+colorPickerIcon.addEventListener('click', function () {
+    // Alternar la clase activa para mostrar/ocultar el menú de colores
+    colorMenu.classList.toggle('active');
+});
+
+// Asigna una función al evento de clic en una opción de color
+colorMenu.addEventListener('click', function (event) {
+    const colorOption = event.target.closest('.color-option');
+    if (colorOption) {
+        const selectedColor = colorOption.dataset.color;
+        const selectedColorAlt = colorOption.dataset.colorAlt;
+
+        // Modifica las variables CSS para el nuevo color y su "alt" correspondiente
+        document.documentElement.style.setProperty('--principal-color', selectedColor);
+        document.documentElement.style.setProperty('--principal-color-alt', selectedColorAlt);
+
+        // Remueve la clase activa para ocultar el menú de colores
+        colorMenu.classList.remove('active');
+    }
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
